c02e39a016834fd7e73a74d9297f777841b34816
Binu <kbinu42@gmail.com>
2019-07-29 18:27:12 +0530

    Updates

---
+++ precache-manifest.e019f1613eeea0dd8f64ddecda7c01e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef118572b0b26876ffa6ed42676b084",
+    "url": "/robo-army/index.html"
+  },
+  {
+    "revision": "b891ef66570fa5b1b223",
+    "url": "/robo-army/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5db9b6988fd910764345",
+    "url": "/robo-army/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b891ef66570fa5b1b223",
+    "url": "/robo-army/static/js/2.93ed5dd5.chunk.js"
+  },
+  {
+    "revision": "5db9b6988fd910764345",
+    "url": "/robo-army/static/js/main.36d634d3.chunk.js"
+  },
+  {
+    "revision": "38ceef2798210bd7a297",
+    "url": "/robo-army/static/js/runtime~main.2cbaed07.js"
+  }
+]);
