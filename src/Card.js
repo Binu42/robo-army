8a8c05fc1984766af22225832297ae0ea82a603f
Binu <kbinu42@gmail.com>
2019-08-02 10:12:56 +0530

    search add

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Card.css'
 
 const Card = ({name, email, id}) => {
     return (
-        <div className='bg-light-blue dib pa3 ma2 grow bw2 shadow-5'>
-        <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
-        <div className='tc'>
-            <h2>{name}</h2>
-            <p className='underline'>{email}</p>
-        </div>
+        <div className='bg-yellow dib pa2 ma2 grow bw2 shadow-5 card'>
+            <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+            <div className='tc'>
+                <h2>{name}</h2>
+                <p className='underline'>{email}</p>
+            </div>
         </div>
     );
 }
